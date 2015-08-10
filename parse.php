1e1491732babf5650d85d0503d7afde88a3a9472
Jesse Donat <donatj@gmail.com>
2015-08-10 15:49:20 -0500

    Adds drop and input-comment arguments

--- parse.php
+++ parse.php
@@ -7,13 +7,24 @@ require('vendor/autoload.php');
 
 $jql = file_get_contents('php://stdin');
 
+$flags        = new donatj\Flags();
+$drop         =& $flags->bool('drop', false, 'Include DROP TABLE?');
+$inputComment =& $flags->bool('input-comment', false, 'Include original input as a comment?');
+
+$flags->parse();
+
+// @todo make availible from mysql-schema
+function escape( $input, $wrapChar = '`' ) {
+	return str_replace($wrapChar, $wrapChar . $wrapChar, $input);
+}
+
 /**
  * @param $jql
  * @throws \Exception
  * @throws \donatj\Misstep\Exceptions\ParseException
  * @throws \donatj\Misstep\Exceptions\StructureException
  */
-function parse( $jql ) {
+function parse( $jql, $drop, $inputComment ) {
 	$jql = preg_replace('%^//.*$%mx', '', $jql); //remove commented lines before parse
 	$jql .= "\n";
 
@@ -221,9 +232,17 @@ function parse( $jql ) {
 		}
 	}
 
+	if( $inputComment ) {
+		$outputJql = rtrim($jql);
+		echo "/*\n{$outputJql}\n*/\n\n";
+	}
+
 	echo "SET FOREIGN_KEY_CHECKS = 0;\n\n";
 	foreach( $tables as $table ) {
 		if( !$table->isIsPseudo() ) {
+			if( $drop ) {
+				echo "DROP TABLE IF EXISTS `" . escape($table->getName()) . "`;\n";
+			}
 			echo $table->toString();
 			echo "\n";
 		}
@@ -235,7 +254,7 @@ try {
 	if( trim($jql) == '' ) {
 		return '';
 	}
-	parse($jql);
+	parse($jql, $drop, $inputComment);
 } catch(\donatj\Misstep\Exceptions\UserException $e) {
 	fwrite(STDERR, "Error: " . $e->getMessage() . "\n");
 	die(1);
